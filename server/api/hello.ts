1bd97dc81e8dcdeccb448857b0dbef3c1c3bec78
pixel-perfect-metodology <35000284+pixel-perfect-metodology@users.noreply.github.com>
2023-03-09 23:14:41 +0700

    remove unused files

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,15 +1,14 @@
-import type { IncomingMessage, ServerResponse } from "http";
+import type { IncomingMessage, ServerResponse } from 'http';
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
   const data = {
-    msg: 'hello 2'
-  }
+    msg: 'hello api world',
+  };
 
-
-  const json = JSON.stringify(data)
-  return data
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
+  const json = JSON.stringify(data);
+  return data;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
   // res.setHeader('Content-length', 'application/json')
-  res.end(json)
-}
+  res.end(json);
+};
