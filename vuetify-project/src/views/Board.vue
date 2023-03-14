79a8f22ce57cd998fd178de3473824f5cd173e74
pixel-perfect-metodology <35000284+pixel-perfect-metodology@users.noreply.github.com>
2023-03-14 11:31:12 +0700

    adjust columns heigh on board page

--- vuetify-project/src/views/Board.vue
+++ vuetify-project/src/views/Board.vue
@@ -1,10 +1,14 @@
 <template>
-  <v-container fluid class="fill-height d-flex align-start">
+  <v-container fluid class="fill-height d-flex align-start pb-0">
     <v-row
-      class="d-flex align-start flex-column"
-      style="flex-wrap: nowrap; overflow-x: scroll"
+      class="d-flex align-start flex-xs-column-and-down"
+      style="
+        flex-wrap: nowrap;
+        overflow-x: scroll;
+        min-height: 100%;
+        height: 100%;
+      "
     >
-      <!-- // todo add  flex-sm-column - as analog flex-column for sm media-query -->
       <template v-for="columnName in columns" :key="columnName">
         <v-col
           cols="auto"
@@ -13,8 +17,7 @@
           md="4"
           sm="6"
           xs="12"
-          class="fill-height"
-          style="position: relative"
+          style="position: relative; min-height: 100%"
         >
           <h2
             class="text-h6 font-weight-medium"
@@ -27,6 +30,7 @@
             <v-sheet
               class="pa-2 bg-grey-lighten-3 text-disabled"
               min-height="150"
+              mi-width="150"
               rounded
             >
               <sub> no tasks </sub>
@@ -95,3 +99,102 @@ const { columns, tasks } = {
   },
 };
 </script>
+
+<style scoped>
+/* .flex-xl-column,
+.flex-lg-column,
+.flex-md-column,
+.flex-sm-column,
+.flex-xs-column {
+  flex-direction: column;
+} */
+
+@media (max-width: 599.98px) {
+  .flex-xs-column-and-down {
+    flex-direction: column;
+  }
+}
+
+/* 
+@media only print {
+  .hidden-print-only {
+    display: none !important;
+  }
+}
+@media only screen {
+  .hidden-screen-only {
+    display: none !important;
+  }
+}
+@media (max-width: 599.98px) {
+  .hidden-xs {
+    display: none !important;
+  }
+}
+@media (min-width: 600px) and (max-width: 959.98px) {
+  .hidden-sm {
+    display: none !important;
+  }
+}
+@media (min-width: 960px) and (max-width: 1279.98px) {
+  .hidden-md {
+    display: none !important;
+  }
+}
+@media (min-width: 1280px) and (max-width: 1919.98px) {
+  .hidden-lg {
+    display: none !important;
+  }
+}
+@media (min-width: 1920px) and (max-width: 2559.98px) {
+  .hidden-xl {
+    display: none !important;
+  }
+}
+@media (min-width: 2560px) {
+  .hidden-xxl {
+    display: none !important;
+  }
+}
+
+@media (min-width: 600px) {
+  .hidden-sm-and-up {
+    display: none !important;
+  }
+}
+@media (min-width: 960px) {
+  .hidden-md-and-up {
+    display: none !important;
+  }
+}
+@media (min-width: 1280px) {
+  .hidden-lg-and-up {
+    display: none !important;
+  }
+}
+@media (min-width: 1920px) {
+  .hidden-xl-and-up {
+    display: none !important;
+  }
+}
+@media (max-width: 959.98px) {
+  .hidden-sm-and-down {
+    display: none !important;
+  }
+}
+@media (max-width: 1279.98px) {
+  .hidden-md-and-down {
+    display: none !important;
+  }
+}
+@media (max-width: 1919.98px) {
+  .hidden-lg-and-down {
+    display: none !important;
+  }
+}
+@media (max-width: 2559.98px) {
+  .hidden-xl-and-down {
+    display: none !important;
+  }
+} */
+</style>
